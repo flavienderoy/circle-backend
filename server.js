b72bfd093e92aa4878549c7852d472b0de17a66a
flavienderoy <flav.deroy@outlook.fr>
2024-04-23 10:59:34 +0200

    fixes for louis

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ const corsOptions = {
 }
 app.use(cors(corsOptions));
 
+app.use('/client', express.static('client'))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
