16044be20094cc821683e7682cfda5a6736fe30b
flavienderoy <flav.deroy@outlook.fr>
2024-04-30 14:35:41 +0200

    variables api

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://" + process.env.DB_USER_PASS + "@localhost:27017/circle-project", { authSource: "admin" } )
+mongoose.connect("mongodb://" + process.env.DB_USER_PASS + "@" + process.env.MONGO_URL + ":27017/circle-project", { authSource: "admin" } )
 .then(() => console.log("Connected to MongoDB"))
 .catch(err => console.log("Failed to connect to MongoDB because :", err));
